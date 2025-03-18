60fd57c0ebc383be35451a9328aa997bd46f0bee
Erick Muke <muke7881@gmail.com>
2025-03-18 01:10:27 -0600

    feat: Se crea estructura de archivos principales

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@styles/index.css'
-import App from '@components/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { App } from "./components/App";
+import "./styles/index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './src/**/*.{html,js,ts,tsx}', // Aquí añades tus archivos .tsx
+    "./src/**/*.{html,js,ts,tsx}", // Aquí añades tus archivos .tsx
   ],
   theme: {
     extend: {},
